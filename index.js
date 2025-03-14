a90158d132a7dd07693c2b323eeaaee60a56d0b9
Manoj Gowda <84260822+ManojGowda89@users.noreply.github.com>
2025-03-14 12:41:44 +0530

    sdjf

--- index.js
+++ index.js
@@ -212,7 +212,8 @@ const whitelist = [
     'http://localhost:3001',
     'http://localhost:4200',
     'http://localhost:8080',
-    'http://localhost:5001'
+    'http://localhost:5001',
+    "https://v-marg2-0.onrender.com"
 ];
 
 const corsOptions = {
